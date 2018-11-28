20fa6c06bc8761d37a26ba20ed2cb252c813b3e3
hewittm2 <35976067+hewittm2@users.noreply.github.com>
2018-11-28 14:53:36 -0600

    All assets added for this sprint

---
+++ RebelStreetWise - MainBuild/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public void CharacterSelectMenu()
+    {
+        SceneManager.LoadScene("CharacterSpawnTest");
+    }
+    public void StageSelectMenu()
+    {
+        SceneManager.LoadScene("StageSelect");
+    }
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void RestartMatch()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
