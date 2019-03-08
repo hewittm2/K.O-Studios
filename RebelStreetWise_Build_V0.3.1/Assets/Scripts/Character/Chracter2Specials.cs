c2e25c73679ee38bb8193d3567beba76c7f73f7e
RyanVanDusen <35843705+RyanVanDusen@users.noreply.github.com>
2019-03-08 14:26:25 -0600

    working back sepcial throw

--- RebelStreetWise_Build_V0.3.1/Assets/Scripts/Character/Chracter2Specials.cs
+++ RebelStreetWise_Build_V0.3.1/Assets/Scripts/Character/Chracter2Specials.cs
@@ -19,83 +19,97 @@ public class Chracter2Specials : FighterClass {
     private GameObject clone;
     private Vector3 spawnPosition;
 
-    public float throwDistance;
-    public float maxThrowDistance;
-    public float maxThrowTime;
+    public float throwSpeed;
+    public float totalThrowTime;
+    public float rotationSpeed;
+
+    private Vector3 rotation;
     private float throwTime;
     private bool throwing = false;
-    private bool retracting = false;
+    private bool throwSetUp = false;
+
+
 
 	// Use this for initialization
 	void Start () {
+
+
         damage = 5;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("B_1") && throwing == false && retracting == false)
+        if(throwing == false)
         {
-            clone = Instantiate(thrownObject);
-            clone.transform.position = thrownObject.transform.position;
-            thrownObject.SetActive(false);
+           
+        }
 
+        //player pushes button
+        if (Input.GetButtonDown("B_1") && throwing == false)
+        {
+        
+            //spawns a clone of the thrownobject gameobject
+            clone = Instantiate(thrownObject);
+            //sets clone position to orginal position
+            clone.transform.position = thrownObject.transform.position;
+            //sets the orginal gameobject to not active (so it appears the object is thrown)
+            thrownObject.SetActive(false);
+            //sets clone's own scale and angles since the orginal is parented to the model
+            clone.transform.localScale = new Vector3(0.5f, 5, 0.5f);
             clone.transform.eulerAngles = new Vector3(0, 0, 0);
+                
             throwing = true;
             
         }
         if (throwing == true)
         {
-          
+           
+            //rotates clone
+            clone.transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+            //starts a counter
             throwTime = throwTime + Time.deltaTime;
-            clone.transform.Translate(new Vector3(1 + (throwDistance * Time.deltaTime), 0, 0));
-            if(throwTime >= maxThrowTime)
+            //moves clone
+            clone.transform.position += (transform.forward *throwSpeed) * Time.deltaTime ;
+            
+            //once the counter is >= to the time set in the inspecter the clone destroys and the orginal object is set back to active
+            if(throwTime >= totalThrowTime)
             {
+                
                 throwing = false;
-                //retracting = true;
+                throwSetUp = false;
                 throwTime = 0;
-                thrownObject.transform.localPosition = (new Vector3(0, 0, 0));
                 Destroy(clone);
                 thrownObject.SetActive(true);
                    
             }
         }
-        //if(retracting == true)
-        //{
-            
-        //    throwTime = throwTime + Time.deltaTime;
-        //    thrownObject.transform.Translate(new Vector3((throwDistance * -1) * Time.deltaTime, 0, 0));
-        //    if (throwTime >= maxThrowTime)
-        //    {
-               
-        //        throwTime = 0;
-        //        retracting = false;
-
-            
-        //    }
-        //}
-
-
-
-
-
     }
 
     void NeutralSpecial()
     {
+        //player presses button
         if (Input.GetButtonDown("B_1"))
         {
+            //checks to see if impower or not
             if (currentEmpowerTime == 0)
             {
+                //buffs damage (value set in inspector)
                 damage = damage * damageBuff;
+                //starts a timer
                 currentEmpowerTime = currentEmpowerTime + Time.deltaTime;
+                //particles
                 empoweredParticle.gameObject.SetActive(true);
             }
         }
+        //once timer is as big as value set in inspector
         if (currentEmpowerTime >= maxEmpowerTime)
         {
+            //takes away damage buff
             damage = damage / damageBuff;
+            //particles end
             empoweredParticle.gameObject.SetActive(false);
+            //resets timer
             currentEmpowerTime = 0;
         }
     }
