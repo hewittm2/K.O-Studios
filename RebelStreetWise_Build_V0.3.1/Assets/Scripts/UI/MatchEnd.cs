380e14bf7355f424ac8bd4f5f59d1fdf43678680
Tkurpiel <31633619+Tkurpiel@users.noreply.github.com>
2019-03-24 20:21:28 -0500

    Update the match end menu and added functionality also added sounds to the pause menu buttons

---
+++ RebelStreetWise_Build_V0.3.1/Assets/Scripts/UI/MatchEnd.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchEnd : MonoBehaviour {
+
+    FighterClass fighterClass;
+    [SerializeField] private Text winnerText;
+    public GameObject EndCanvas;
+    public GameObject Event1;
+    public GameObject Event3;
+
+	public void Winner (int teamNumber)
+    {
+        StartCoroutine(MatchEnder(teamNumber));
+        //Creats slow motion
+        Time.timeScale = 0.5f;
+    }
+    private IEnumerator MatchEnder(int teamNmber)
+    {
+        // Displays the Text before the match ends
+        if(teamNmber == 1){
+            winnerText.text = "Team 2 Wins";
+        }
+        if (teamNmber == 2){
+            winnerText.text = "Team 1 Wins";
+        }
+        yield return new WaitForSeconds(3);
+        //Brings Up the Match End Screen and stops time so other people cannot move
+        Time.timeScale = 0;
+        EndCanvas.SetActive(true);
+        if (teamNmber == 1){
+            Event3.SetActive(true);
+        }
+        if (teamNmber == 2){
+            Event1.SetActive(true);
+        }
+    }
+}
