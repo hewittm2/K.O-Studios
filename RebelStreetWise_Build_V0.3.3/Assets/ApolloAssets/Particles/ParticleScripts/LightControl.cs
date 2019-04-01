f5fa486012991dace99e7e45f9ee8398698ade40
Mitchell Hewitt <hewittm2@gmatc.matc.edu>
2019-04-01 13:30:00 -0500

    Trying to fix this with Jake's Build

---
+++ RebelStreetWise_Build_V0.3.3/Assets/ApolloAssets/Particles/ParticleScripts/LightControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour
+{
+    public float nRand = 0;
+	void Update ()
+    {
+        nRand = Random.Range(4f, 5f);
+        this.transform.GetComponent<Light>().intensity = nRand;
+	}
+}
---
+++ RebelStreetWise_Build_V0.3.3/Assets/Scripts/Environment/PanelV2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelV2 : MonoBehaviour
+{
+    public List<Material> colors = new List<Material>();
+
+    public float seconds = 1;
+    public int start;
+
+    private int c;
+
+    private bool waiting = true;
+
+    private void Start()
+    {
+        c = start;
+
+        gameObject.GetComponent<Renderer>().material = colors[start - 1];
+        StartCoroutine(Wait());
+    }
+
+    private void Update()
+    {
+        if (!waiting)
+        {
+            if (c < colors.Count)
+            {
+                c++;
+                gameObject.GetComponent<Renderer>().material = colors[c - 1];
+                StartCoroutine(Wait());
+            }
+            else if (c == colors.Count)
+            {
+                c = 1;
+                gameObject.GetComponent<Renderer>().material = colors[c - 1];
+                StartCoroutine(Wait());
+            }
+        }
+        else
+            return;
+    }
+
+    private IEnumerator Wait()
+    {
+        waiting = true;
+        yield return new WaitForSeconds(seconds);
+        waiting = false;
+    }
+}
