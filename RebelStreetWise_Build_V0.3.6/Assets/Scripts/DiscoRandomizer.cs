4fb1fe3aa17f8ef3577587f2e6f8b78f5fa1ec1a
hewittm2 <35976067+hewittm2@users.noreply.github.com>
2019-04-23 10:31:44 -0500

    Remove Solaris lounge for imple

---
+++ RebelStreetWise_Build_V0.3.6/Assets/Scripts/DiscoRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoRandomizer : MonoBehaviour
+{
+    public List<DiscoPanelColor> discoPanels = new List<DiscoPanelColor>();
+
+    private void Start()
+    {
+        foreach (DiscoPanelColor dpc in discoPanels)
+        {
+            dpc.start = Random.Range(0, 5);
+        }
+    }
+}
---
+++ RebelStreetWise_Build_V0.3.6/Assets/Scripts/DiscoRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoRotation : MonoBehaviour
+{
+    public int rotationSpeed;
+	
+	void Update ()
+    {
+        transform.Rotate(0, rotationSpeed*Time.deltaTime, 0);
+	}
+}
