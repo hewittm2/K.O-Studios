f01dc91ab99b6cc96229f73c87780493cd61cf52
RyanVanDusen <35843705+RyanVanDusen@users.noreply.github.com>
2019-05-03 17:07:38 -0500

    fixed spawn positions in groovy

--- RebelStreetWise_Build_V0.3.9/Assets/Scripts/Character/CharacterScaling.cs
+++ RebelStreetWise_Build_V0.3.9/Assets/Scripts/Character/CharacterScaling.cs
@@ -7,6 +7,11 @@ public class CharacterScaling : MonoBehaviour {
     public GameObject Character2;
     public GameObject Character3;
     public GameObject Character4;
+
+    public GameObject spawn1;
+    public GameObject spawn2;
+    public GameObject spawn3;
+    public GameObject spawn4;
     public Vector3 Size = new Vector3(.2f,.2f,.2f);
 	// Use this for initialization
 	void Start () {
@@ -20,10 +25,12 @@ public class CharacterScaling : MonoBehaviour {
         Character3.transform.localScale = Size;
         Character4.transform.localScale = Size;
 
-        //Character1.transform.eulerAngles = new Vector3(0, 0, 0);
-        //Character2.transform.eulerAngles = new Vector3(0, 0, 0);
-        //Character3.transform.eulerAngles = new Vector3(0, 0, 0);
-        //Character4.transform.eulerAngles = new Vector3(0, 0, 0);
+        Character1.transform.position = spawn1.transform.position;
+        Character2.transform.position = spawn2.transform.position;
+        Character3.transform.position = spawn3.transform.position;
+        Character4.transform.position = spawn4.transform.position;
+
+      
     }
 	
 	// Update is called once per frame
