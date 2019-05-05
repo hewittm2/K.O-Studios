43e7cea023a05ea5e0f4c115624f2ba1103687c2
TorrelLloyd <36899107+TorrelLloyd@users.noreply.github.com>
2019-05-05 16:20:44 -0500

    Torrel's Apollo

--- RebelStreetWise_Build_V0.3.9/Assets/Scripts/Character/HitDetection.cs
+++ RebelStreetWise_Build_V0.3.9/Assets/Scripts/Character/HitDetection.cs
@@ -244,14 +244,6 @@ public class HitDetection : MonoBehaviour {
 		yield return new WaitForSeconds(.01f);
 		movement.rigid.constraints = RigidbodyConstraints.FreezeAll;
 
-		if (attacker.coupDeGraceActivated) {
-			for (int i = 0; i < 100; i++) {
-				yield return new WaitForSeconds (player.anim.GetCurrentAnimatorClipInfo (0).Length / player.anim.speed);
-				player.anim.SetTrigger("Heavy Damage");
-			}
-		
-		}
-
 
 		yield return new WaitForSeconds (.2f);
 		attacker = null;
