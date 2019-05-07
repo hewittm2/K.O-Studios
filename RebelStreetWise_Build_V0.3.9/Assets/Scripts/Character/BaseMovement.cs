cc32048ee33119865cecbd90fc133fda5715c7a3
RyanVanDusen <35843705+RyanVanDusen@users.noreply.github.com>
2019-05-07 09:58:58 -0500

    Fightline fixes

--- RebelStreetWise_Build_V0.3.9/Assets/Scripts/Character/BaseMovement.cs
+++ RebelStreetWise_Build_V0.3.9/Assets/Scripts/Character/BaseMovement.cs
@@ -57,10 +57,13 @@ public class BaseMovement : MonoBehaviour
             heightArray[i] = character.height / (i + 1);
             centerOffset = 0.25f;
         }
-
+        fightLine = GameObject.Find("FightLine");
+        rightBoundingWall = GameObject.Find("RightBoundary");
+        leftBoundingWall = GameObject.Find("LeftBoundary");
         rend = fightLine.GetComponent<Renderer>();
         leftWallRend = leftBoundingWall.GetComponent<Renderer>();
         rightWallRend = rightBoundingWall.GetComponent<Renderer>();
+
     }
 
     private void Update(){
