00b1b9461f91efb3a0d2d8f00a9ceb58ca0bb6c1
RyanVanDusen <35843705+RyanVanDusen@users.noreply.github.com>
2019-05-07 09:27:31 -0500

    Revert "FinalMerge(4/30)"
    This reverts commit e7fda3d44003e6e470bef62b017844d12ba08e4e.

--- RebelStreetWise_Build_V0.3.9/Assets/Scripts/Character/HandAnimation.cs
+++ RebelStreetWise_Build_V0.3.9/Assets/Scripts/Character/HandAnimation.cs
@@ -23,15 +23,6 @@ public class HandAnimation : MonoBehaviour {
 	}
 	public Hand myHand = Hand.Right;
 
-	private enum HandState{
-		Open,
-		Fist,
-		FingerGun,
-		Transition
-	}
-	[SerializeField]
-	private HandState CurrentHandState = HandState.Open;
-
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < 4; i++) {
@@ -53,34 +44,28 @@ public class HandAnimation : MonoBehaviour {
 ////MakeFist/FingerGun
 		/// 
 		if (MakeFist) {
-			if (CurrentHandState != HandState.Fist) {
-				CurrentHandState = HandState.Transition;
 //loop for finger parent rotation
-				//fc.anim.GetCurrentAnimatorClipInfo().Length
-				for (int i = 0; i < 4; i++) {
+			//fc.anim.GetCurrentAnimatorClipInfo().Length
+			for (int i = 0; i < 4; i++) {
 				
-					Fingers [i].localEulerAngles = new Vector3 (Mathf.LerpAngle (Fingers [i].localEulerAngles.x, FistAngle, t), Fingers [i].localEulerAngles.y, Fingers [i].localEulerAngles.z);
+				Fingers [i].localEulerAngles = new Vector3 (Mathf.LerpAngle(Fingers [i].localEulerAngles.x, FistAngle, t), Fingers [i].localEulerAngles.y, Fingers [i].localEulerAngles.z);
 //loop for digit rotation
-					for (int j = 0; j < 2; j++) {
-						Digits [i] [j].localEulerAngles = new Vector3 (Mathf.LerpAngle (Fingers [i].localEulerAngles.x, FistAngle, t), Digits [i] [j].localEulerAngles.y, Digits [i] [j].localEulerAngles.z);
-					}
+				for (int j = 0; j < 2; j++) {
+					Digits[i][j].localEulerAngles = new Vector3 (Mathf.LerpAngle(Fingers [i].localEulerAngles.x, FistAngle, t), Digits[i][j].localEulerAngles.y, Digits[i][j].localEulerAngles.z);
 				}
-//thumb control
-				for (int x = 1; x < 2; x++) {
-					if (myHand == Hand.Left)
-						ThumbDigits [x].localEulerAngles = new Vector3 (ThumbDigits [x].localEulerAngles.x, ThumbDigits [x].localEulerAngles.y, Mathf.LerpAngle (ThumbDigits [x].localEulerAngles.z, FistAngle, t));
-					else
-						ThumbDigits [x].localEulerAngles = new Vector3 (ThumbDigits [x].localEulerAngles.x, ThumbDigits [x].localEulerAngles.y, Mathf.LerpAngle (ThumbDigits [x].localEulerAngles.z, -FistAngle, t));
-				}
-				t += Speed * 0.1f;
-			} else {
-				MakeFist = false;
 			}
+//thumb control
+			for (int x = 1; x < 2; x++) {
+				if(myHand == Hand.Left)
+					ThumbDigits[x].localEulerAngles = new Vector3 (ThumbDigits[x].localEulerAngles.x, ThumbDigits[x].localEulerAngles.y, Mathf.LerpAngle(ThumbDigits[x].localEulerAngles.z, FistAngle, t));
+				else
+					ThumbDigits[x].localEulerAngles = new Vector3 (ThumbDigits[x].localEulerAngles.x, ThumbDigits[x].localEulerAngles.y, Mathf.LerpAngle(ThumbDigits[x].localEulerAngles.z, -FistAngle, t));
+			}
+			t += Speed * 0.1f;
 
 
 
 		} else if(FingerGun){
-			CurrentHandState = HandState.Transition;
 //loop for finger parent rotation
 			for (int x = 0; x < 2; x++) {
 				Fingers [x].localEulerAngles = new Vector3 (Mathf.LerpAngle (Fingers [x].localEulerAngles.x, 0, t), Fingers [x].localEulerAngles.y, Fingers [x].localEulerAngles.z);
@@ -105,7 +90,6 @@ public class HandAnimation : MonoBehaviour {
 
 ////Release fist/fingergun
 		if (releaseFist) {
-			CurrentHandState = HandState.Transition;
 //loop for finger parent rotation
 			for (int i = 0; i < 4; i++) {
 
@@ -123,7 +107,6 @@ public class HandAnimation : MonoBehaviour {
 
 //release
 		} else if(ReleaseFingerGun){
-			CurrentHandState = HandState.Transition;
 //loop for finger parent rotation
 			for (int i = 2; i < 4; i++) {
 				Fingers [i].localEulerAngles = new Vector3 (Mathf.LerpAngle(FistAngle, 0, t), Fingers [i].localEulerAngles.y, Fingers [i].localEulerAngles.z);
@@ -140,57 +123,8 @@ public class HandAnimation : MonoBehaviour {
 
 			t += Speed * 0.1f;
 		}
-
-
-
-//keeps fingers in place while not in transition state
-		if (t == 0) {
-			switch(CurrentHandState){
-			case HandState.Fist:
-				for (int i = 0; i < 4; i++) {
-					Fingers [i].localEulerAngles = new Vector3 (Mathf.LerpAngle(Fingers [i].localEulerAngles.x, FistAngle, 1), Fingers [i].localEulerAngles.y, Fingers [i].localEulerAngles.z);
-					//loop for digit rotation
-					for (int j = 0; j < 2; j++) {
-						Digits[i][j].localEulerAngles = new Vector3 (Mathf.LerpAngle(Fingers [i].localEulerAngles.x, FistAngle, 1), Digits[i][j].localEulerAngles.y, Digits[i][j].localEulerAngles.z);
-					}
-				}
-				//thumb control
-				for (int x = 1; x < 2; x++) {
-					if(myHand == Hand.Left)
-						ThumbDigits[x].localEulerAngles = new Vector3 (ThumbDigits[x].localEulerAngles.x, ThumbDigits[x].localEulerAngles.y, Mathf.LerpAngle(ThumbDigits[x].localEulerAngles.z, FistAngle, 1));
-					else
-						ThumbDigits[x].localEulerAngles = new Vector3 (ThumbDigits[x].localEulerAngles.x, ThumbDigits[x].localEulerAngles.y, Mathf.LerpAngle(ThumbDigits[x].localEulerAngles.z, -FistAngle, 1));
-				}
-				break;
-			case HandState.FingerGun:
-				for (int x = 0; x < 2; x++) {
-					Fingers [x].localEulerAngles = new Vector3 (Mathf.LerpAngle (Fingers [x].localEulerAngles.x, 0, 1), Fingers [x].localEulerAngles.y, Fingers [x].localEulerAngles.z);
-					for (int j = 0; j < 2; j++) {
-						Digits [x] [j].localEulerAngles = new Vector3 (Mathf.LerpAngle (Fingers [x].localEulerAngles.x, 0, 1), Digits [x] [j].localEulerAngles.y, Digits [x] [j].localEulerAngles.z);
-					}
-				}
-				for (int i = 2; i < 4; i++) {
-					Fingers [i].localEulerAngles = new Vector3 (Mathf.LerpAngle(Fingers [i].localEulerAngles.x, FistAngle, 1), Fingers [i].localEulerAngles.y, Fingers [i].localEulerAngles.z);
-					//loop for digit rotation
-					for (int j = 0; j < 2; j++) {
-						Digits[i][j].localEulerAngles = new Vector3 (Mathf.LerpAngle(Fingers [i].localEulerAngles.x, FistAngle, 1), Digits[i][j].localEulerAngles.y, Digits[i][j].localEulerAngles.z);
-					}
-				}
-				break;
-			default :
-				break;
-			}
-		}
 //val reset
 		if (t > 1 || t == 1) {
-
-			if (MakeFist)
-				CurrentHandState = HandState.Fist;
-			if (FingerGun)
-				CurrentHandState = HandState.FingerGun;
-			if (releaseFist || ReleaseFingerGun)
-				CurrentHandState = HandState.Open;
-			
 			MakeFist = false;
 			FingerGun = false;
 			ReleaseFingerGun = false;
